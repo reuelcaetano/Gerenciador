0b99453c0efc7e87c4c53a831da155b026bc1c7b
Madson Paulo <madson-paulo@hotmail.com>
2017-12-02 17:53:13 -0200

    Update Gerenciador.scala
    Adição de recursividade;
    Remoção da variável mutável loop;

--- src/main/scala/view/Gerenciador.scala
+++ src/main/scala/view/Gerenciador.scala
@@ -1,22 +1,32 @@
 package view
 
-import model._
+import model.{Cardapio, Pedido, Cadastro, Promocao, Relatorio}
 
 object Main {
   def main(args: Array[String]) {
-    var loop = 1
-    while(loop == 1) {
-      val input = scala.io.StdIn.readLine("[0] Cardapio | [1] Pedido | [2] Cadastrar | [3] Remover | [4] Alterar | [5] Promocao | [6] Relatorio | [7] Sair: ")
-      input match {
-        case "0" => println(Cardapio.cardapio())    // Exibe cardapio de itens cadastrados
-        case "1" => println(Pedido.pedido())        // Chama opcoes para novoPedido() / cancelarPedido()
-        case "2" => println(Cadastro.cadastrar())   // Cadastra novo produto
-        case "3" => println(Cadastro.remover())     // Remove produto cadastrado
-        case "4" => println(Cadastro.alterar())     // Altera valores especificos de produtos cadastrados
-        case "5" => println(Promocao.promover())    // Altera o preco de um produto para deixa-lo com desconto
-        case "6" => println(Relatorio.visualizar()) // Exibe relatorio de vendas efetuadas
-        case "7" => loop = 0                        // Seta variavel para finalizar loop
-      }
+    showMenu()
+  }
+
+  def showMenu(): Unit ={
+    val input = scala.io.StdIn.readLine("[0] Cardapio | [1] Pedido | [2] Cadastrar | [3] Remover | [4] Alterar | [5] Promocao | [6] Relatorio | [7] Sair: ")
+    input match {
+      case "0" => println(Cardapio.cardapio())
+        showMenu();// Exibe cardapio de itens cadastrados
+      case "1" => println(Pedido.pedido())        // Chama opcoes para novoPedido() / cancelarPedido()
+        showMenu();
+      case "2" => println(Cadastro.cadastrar())   // Cadastra novo produto
+        showMenu();
+      case "3" => println(Cadastro.remover())     // Remove produto cadastrado
+        showMenu();
+      case "4" => println(Cadastro.alterar())     // Altera valores especificos de produtos cadastrados
+        showMenu();
+      case "5" => println(Promocao.promover())    // Altera o preco de um produto para deixa-lo com desconto
+        showMenu();
+      case "6" => println(Relatorio.visualizar()) // Exibe relatorio de vendas efetuadas
+        showMenu();
+      case "7" => println("Sistema encerrado com sucesso")
+      case _ => println("Comando inválido")
+        showMenu()
     }
   }
 }
