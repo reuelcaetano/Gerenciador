ee6bc58256828cbab45c6d3407614ad12c8925b8
Madson Paulo <madson-paulo@hotmail.com>
2017-12-03 17:30:07 -0200

    Update Pedido.scala
    Migração de paradigma imperativo para funcional

--- src/main/scala/model/Pedido.scala
+++ src/main/scala/model/Pedido.scala
@@ -4,40 +4,50 @@ import java.util.GregorianCalendar
 
 object Pedido {
 
-  // Funcao para setar opcao para novoPedido() / cancelarPedido()
-  def pedido() : String = {
+  // Funcao para criar um novo pedido ou cancelar um pedido existente
+  def pedido(): String = {
     val input = scala.io.StdIn.readLine("[0] Novo Pedido | [1] Cancelar Pedido: ")
     input match {
-      case "0" => novoPedido()
-      case "1" => cancelarPedido()
+      case "0" =>
+        val cardapio = FileIO.ler("menu.txt", "").split("@")
+        val id = FileIO.ler("pedidoID.txt", "1")
+        novoPedido(cardapio, id)
+      case "1" =>
+        val pedidos = FileIO.ler("pedido.txt", "").split("@")
+        cancelarPedido(pedidos)
     }
   }
 
   // Funcao para efetuar um novo pedido
-  def novoPedido() : String = {
-    println(Cardapio.cardapio())
-    val id = FileIO.ler("pedidoID.txt", "1")
-    val quant = scala.io.StdIn.readLine("Quantidade: ")
-    val sabor = scala.io.StdIn.readLine("Sabor: ")
-    val tam = scala.io.StdIn.readLine("Tamanho[P,M,G]: ")
-    FileIO.escrever("pedido.txt", id+"#"+new GregorianCalendar().getTime.toString+"#"+quant+"x "+sabor+" ["+tam+"]"+"@")
-    FileIO.subescrever("pedidoID.txt", (id.toInt+1).toString)
-    return "Pedido realizado com sucesso!"
+  def novoPedido(cardapio: Array[String], id: String): String = {
+    //imprime o cardápio
+    Cardapio.cardapio(cardapio)
+    val produto = scala.io.StdIn.readLine("ID do produto: ")
+    // verifica se o produto existe
+    if (Cadastro.existeProduto(cardapio, produto)) {
+      val quant = scala.io.StdIn.readLine("Quantidade: ")
+      FileIO.escrever("pedido.txt", id + "#" + new GregorianCalendar().getTime.toString + "#" + quant + " x " + Cadastro.getProduto(cardapio, produto) + "@")
+      FileIO.subescrever("pedidoID.txt", (id.toInt + 1).toString)
+      return "Pedido realizado com sucesso!"
+    } else {
+      return "ID informado não existe."
+    }
   }
 
   // Funcao para cancelar pedido cadastrado
-  def cancelarPedido() : String = {
-    println(Relatorio.visualizar())
-    val id = scala.io.StdIn.readLine("Remover ID: ")
-    val pedidos = FileIO.ler("pedido.txt","").split("@")
-    var lista = ""
-    for (i <- 0 to pedidos.length-1) {
-      var itens = pedidos(i).split("#")
-      if (id != itens(0)){
-        lista = lista+itens(0)+"#"+itens(1)+"#"+itens(2)+"#"+"@"
+  def cancelarPedido(pedidos: Array[String]): String = {
+    // mostra os pedidos cadastrados
+    println(Relatorio.visualizar(pedidos))
+    val id = scala.io.StdIn.readLine("Número do pedido a ser cancelado: ")
+    if (Cadastro.existeProduto(pedidos, id)) {
+      var lista = ""
+      pedidos.foreach { e =>
+        if (e.split("#")(0) != id) lista += (e + "@")
       }
       FileIO.subescrever("pedido.txt", lista)
+      return "Pedido cancelado com sucesso!"
+    } else {
+      return "Numero do pedido não existe."
     }
-    return "Pedido cancelado com sucesso!"
   }
 }
