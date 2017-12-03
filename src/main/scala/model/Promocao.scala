0cbb63cd15b66c692f6ed1b255e65a46c683c58b
Madson Paulo <madson-paulo@hotmail.com>
2017-12-03 17:31:53 -0200

    Update Promocao.scala
    Migração de paradigma imperativo para funcional

--- src/main/scala/model/Promocao.scala
+++ src/main/scala/model/Promocao.scala
@@ -3,22 +3,25 @@ package model
 object Promocao {
 
   // Funcao para alterar valor de preco atual por um valor com desconto
-  def promover(): String = {
-    println(Cardapio.cardapio())
-    val id = scala.io.StdIn.readLine("Promover ID: ")
-    val cardapio = FileIO.ler("menu.txt","").split("@")
-    var produtos = ""
-    for (i <- 0 to cardapio.length - 1) {
-      var itens = cardapio(i).split("#")
-      if (id != itens(0)) {
-        produtos = produtos + itens(0) + "#" + itens(1) + "#" + itens(2) + "#" + itens(3) + "@"
+  def promover(cardapio: Array[String]): String = {
+    // imprime o cardápio
+    Cardapio.cardapio(cardapio)
+    val id = scala.io.StdIn.readLine("ID do produto a ser promovido: ")
+    if (Cadastro.existeProduto(cardapio, id)) {
+      var produtos = ""
+      cardapio.foreach { e =>
+        if (e.split("#")(0) != id) {
+          produtos += (e + "@")
+        } else {
+          val data = e.split("#")
+          val input = scala.io.StdIn.readLine("Novo preço promocional: ")
+          produtos = produtos + data(0) + "#" + data(1) + "#" + data(2) + "#" + input + "*@"
+        }
       }
-      if (id == itens(0)) {
-        val input = scala.io.StdIn.readLine("Promocao: ")
-        produtos = produtos + itens(0) + "#" + itens(1) + "#" + itens(2) + "#" + input+"*"+ "@"
-      }
+      FileIO.subescrever("menu.txt", produtos)
+      return "Item promovido!"
+    } else {
+      return "ID informado não existe"
     }
-  FileIO.subescrever("menu.txt", produtos)
-  return "Item promovido!"
   }
 }
