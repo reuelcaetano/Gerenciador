3389454532615e929d9f8f2724f7c80e7b4b6cf6
Madson Paulo <madson-paulo@hotmail.com>
2017-12-03 17:29:22 -0200

    Update Cardapio.scala
    Migração de paradigma imperativo para funcional

--- src/main/scala/model/Cardapio.scala
+++ src/main/scala/model/Cardapio.scala
@@ -1,24 +1,19 @@
 package model
 
+import scala.annotation.tailrec
+
 object Cardapio {
 
   // Exibe cardapio de itens cadastrados
-  def cardapio() : String = {
-      val cardapio = FileIO.ler("menu.txt","").split("@")
-      var produtos = ""
-      if (cardapio(0) != "") {
-        for (i <- 0 to cardapio.length - 1) {
-          var itens = cardapio(i).split("#")
-          produtos = produtos + "ID:" + itens(0) + " " + itens(1).toUpperCase + " [" + itens(2).toUpperCase + "]" + " R$" + itens(3) + "\n"
-        }
-        return "\n[CARDAPIO]\n----------------------------\n" + produtos
-      }
-      return "Cardapio vazio!"
-    }
+  def cardapio(cardapio: Array[String]): Unit = {
+    println("\n[CARDÁPIO]\n----------------------------")
+    cardapio.foreach(e => println(criaProduto(e)))
+    println("* Preço promocional por tempo limitado\n----------------------------")
+  }
 
-  // Retorna quantidade total de produtos cadastrados
-  def totalProdutos() : Int = {
-      val cardapio = FileIO.ler("menu.txt","").split("@")
-      return cardapio.length
+  // formata os dados para exibição de um produto
+  def criaProduto(produtoTxt: String): String = {
+    var produto = produtoTxt.split("#")
+    return "ID: " + produto(0) + ", " + produto(1).toUpperCase + " [" + produto(2) + "], R$ " + produto(3)
   }
 }
